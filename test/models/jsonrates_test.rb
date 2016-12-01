0d3ec3ed3b8c99e09786f426f91ccdacd878bb94
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-12-01 15:47:22 -0200

    Test to not supported rates

--- test/models/jsonrates_test.rb
+++ test/models/jsonrates_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class JsonratesTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup do
+    @not_supported = "Don't support this action yet!"
+  end
+
+  test "don't support convert action yet" do
+    jsonrates = Jsonrates.new(:convert)
+    rates = jsonrates.load
+    assert_equal rates, @not_supported
+  end
+
+  test "don't support timeframe action yet" do
+    jsonrates = Jsonrates.new(:timeframe)
+    rates = jsonrates.load
+    assert_equal rates, @not_supported
+  end
+
+  test "don't support change action yet" do
+    jsonrates = Jsonrates.new(:change)
+    rates = jsonrates.load
+    assert_equal rates, @not_supported
+  end
 end
