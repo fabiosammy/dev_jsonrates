8eb27d7cbaab1dd0a15e5c2428153f5a45acd9a7
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-12-02 19:51:56 -0200

    Updated controller to added a method for categories_params

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,28 @@
 class HomeController < ApplicationController
   def index
-    # @jsonrates = Jsonrates.new
     @stocks = Stock.where(on: (Date.today - 30.days)..Date.today)
-    @data_stocks = load_categories(params[:categories])
-    @data_stocks.push @stocks.to_series if @data_stocks.empty?
+    @categories = load_categories(params[:categories])
+    @data_stocks = load_categories_series(@categories)
   end
 
   private
 
   def load_categories(categories_params)
-    return [] if categories_params.nil?
+    if categories_params.kind_of?(Array)
+      categories_params
+    elsif categories_params.nil?
+      []
+    else
+      [categories_params]
+    end
+  end
+
+  def load_categories_series(categories_params)
     data_stocks = []
     categories_params.each do |category|
       data_stocks.push @stocks.to_series(category.to_sym)
     end
+    data_stocks.push @stocks.to_series if data_stocks.empty?
     data_stocks
   end
 end
