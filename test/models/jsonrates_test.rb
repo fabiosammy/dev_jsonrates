390d3bfb3036b7ad9eb4538080e23852f8665dbf
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-12-02 00:36:03 -0200

    Update jsonrates to find data by date

--- test/models/jsonrates_test.rb
+++ test/models/jsonrates_test.rb
@@ -5,23 +5,23 @@ class JsonratesTest < ActiveSupport::TestCase
     @not_supported = "Don't support this action yet!"
   end
 
-  test "don't support convert action yet" do
-    jsonrates = Jsonrates.new(:convert)
-    rates = jsonrates.load_data
-    assert_equal rates, @not_supported
-  end
-
-  test "don't support timeframe action yet" do
-    jsonrates = Jsonrates.new(:timeframe)
-    rates = jsonrates.load_data
-    assert_equal rates, @not_supported
-  end
-
-  test "don't support change action yet" do
-    jsonrates = Jsonrates.new(:change)
-    rates = jsonrates.load_data
-    assert_equal rates, @not_supported
-  end
+  # test "don't support convert action yet" do
+  #   jsonrates = Jsonrates.new(:convert)
+  #   rates = jsonrates.load_data
+  #   assert_equal rates, @not_supported
+  # end
+  #
+  # test "don't support timeframe action yet" do
+  #   jsonrates = Jsonrates.new(:timeframe)
+  #   rates = jsonrates.load_data
+  #   assert_equal rates, @not_supported
+  # end
+  #
+  # test "don't support change action yet" do
+  #   jsonrates = Jsonrates.new(:change)
+  #   rates = jsonrates.load_data
+  #   assert_equal rates, @not_supported
+  # end
 
   test "Jsonrates isn't persisted" do
     jsonrates = Jsonrates.new
