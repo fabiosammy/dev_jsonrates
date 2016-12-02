f1c80adc9824e545ffaee68f5b548ccda1f181e1
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-12-02 20:26:19 -0200

    Added last update of stocks information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :get_last_stock
+
+  private
+
+  def get_last_stock
+    @stock = Stock.order(updated_at: :desc).first
+  end
 end
