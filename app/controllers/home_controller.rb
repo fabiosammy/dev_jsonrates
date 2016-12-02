922607eeae1e25130a687ec6bfa27d6a34b99c37
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-12-02 13:04:40 -0200

    Added method on home_controller to load data series of stocks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
   def index
-    @jsonrates = Jsonrates.new
+    # @jsonrates = Jsonrates.new
+    @stocks = Stock.where(on: (Date.today - 30.days)..Date.today)
+    @data_stocks = load_categories(params[:categories])
+    @data_stocks.push @stocks.to_series if @data_stocks.empty?
+  end
+
+  private
+
+  def load_categories(categories_params)
+    return [] if categories_params.nil?
+    data_stocks = []
+    categories_params.each do |category|
+      data_stocks.push @stocks.to_series(category.to_sym)
+    end
+    data_stocks
   end
 end
