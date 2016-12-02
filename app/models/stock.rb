37e0ed8b5e658393e2a1b1407db3b94632616b36
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-12-02 21:07:46 -0200

    Fixed stocks to load from api

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,12 +3,13 @@ class Stock < ApplicationRecord
   serialize :data, JSON
 
   def self.load_api(start_at = nil, end_at = Date.today)
-    start_at = end_at - 30.days if start_at.nil?
+    return nil if ENV['ENDPOINT_API_KEY'].nil?
+    start_at = (end_at - 30.days) if start_at.nil?
     (start_at..end_at).each do |date|
       jsonrates = Jsonrates.new(date)
       on = jsonrates.date
       data = jsonrates.loaded_data
-      stock = Stock.first_or_initialize on: on
+      stock = Stock.where(on: on).first_or_initialize
       stock.data = data
       stock.save!
     end
